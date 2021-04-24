1a2e8f01ce18873c6b126fa8b4267374fa3e8ede
Leben Asa <lebenasa@hotmail.com>
2021-04-24 19:29:42 +0700

    Add typography guideline

--- src/components/page/Design.tsx
+++ src/components/page/Design.tsx
@@ -1,7 +1,9 @@
-export interface Props {
+import Grid from "../layout/Grid";
+
+export interface DesignProps {
 }
 
-export default function (props: Props) {
+export default function (props: DesignProps) {
     return (
         <>
             <h1>
@@ -10,6 +12,38 @@ export default function (props: Props) {
             <h2>
                 Typography
             </h2>
+            <Grid columns={4}>
+                <Grid.Item>
+                    <h1 style={{ margin: 0 }}>Header 1</h1>
+                </Grid.Item>
+                    <Grid.Item columnSpan={3}>
+                        <p>Header 1</p>
+                </Grid.Item>
+                <Grid.Item>
+                    <h2 style={{ margin: 0 }}>Header 2</h2>
+                </Grid.Item>
+                <Grid.Item columnSpan={3}>
+                    <p>Header 2</p>
+                </Grid.Item>
+                <Grid.Item>
+                    <p>Normal text</p>
+                </Grid.Item>
+                <Grid.Item columnSpan={3}>
+                    <p>Normal text</p>
+                </Grid.Item>
+                <Grid.Item>
+                    <a href="#">Link</a>
+                </Grid.Item>
+                <Grid.Item columnSpan={3}>
+                    <p>Link</p>
+                </Grid.Item>
+                <Grid.Item>
+                    <code>Code block</code>
+                </Grid.Item>
+                <Grid.Item columnSpan={3}>
+                    <p>Code block</p>
+                </Grid.Item>
+            </Grid>
             <h2>
                 Colour Palette
             </h2>
